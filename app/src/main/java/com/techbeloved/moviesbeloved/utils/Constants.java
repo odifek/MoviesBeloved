02dba7e489f03798a7d5689a16585fde9a748920
odifek <kennedy998@gmail.com>
2018-09-25 13:02:36 +0100

    update dependencies and put api keys in external gradle.properties to avoid public disclosure

--- app/src/main/java/com/techbeloved/moviesbeloved/utils/Constants.java
+++ app/src/main/java/com/techbeloved/moviesbeloved/utils/Constants.java
@@ -1,12 +1,14 @@
 package com.techbeloved.moviesbeloved.utils;
 
+import com.techbeloved.moviesbeloved.BuildConfig;
+
 public class Constants {
     public static final String MOVIE_ID_EXTRA = "movie_id";
     public static final String TMDB_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
     public static final String TMDB_API_BASE_URL = "https://api.themoviedb.org/3/";
     public static final String DEFAULT_POSTER_SIZE = "w185";
     public static final String DEFAULT_BACKDROP_SIZE = "w780";
-    public static final String TMDB_API_KEY = "b8eb7211b6e1882884bdf92f1d94961a";
+    public static final String TMDB_API_KEY = BuildConfig.TmdbApiKey;
     public static final String PAGE_QUERY_PARAM = "page";
     public static final String API_KEY_QUERY_PARAM = "api_key";
     public static final String MOVIE_PATH_SEG = "movie";
